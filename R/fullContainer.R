96b96f4b58bef63f3f9b3a46042820f624eb00d6
John Coene <jcoenep@gmail.com>
2018-01-07 13:23:56 +0100

    container examples

--- R/fullContainer.R
+++ R/fullContainer.R
@@ -1,4 +1,47 @@
-#' Add a container
+#' Add containers
+#'
+#' Add a container.
+#'
+#' @details Pass within your grid, sections or slides.
+#'
+#' @examples
+#' if(interactive()){
+#'   library(shiny)
+#'
+#'   ui <- fullPage(
+#'     center = TRUE,
+#'     fullSection(
+#'       h1("Containers")
+#'     ),
+#'     fullSection(
+#'       h1("Without container"),
+#'       plotOutput("plot1")
+#'     ),
+#'     fullSection(
+#'       fullContainer(
+#'         h1("With container"),
+#'         plotOutput("plot2"),
+#'         pre(
+#'           code(
+#'             "fullContainer()"
+#'           )
+#'         )
+#'       )
+#'     )
+#'   )
+#'
+#'   server <- function(input, output){
+#'     output$plot1 <- renderPlot({
+#'       plot(mtcars$wt, mtcars$mpg)
+#'     })
+#'
+#'     output$plot2 <- renderPlot({
+#'       plot(mtcars$wt, mtcars$mpg)
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#' }
 #'
 #' @param ... any element.
 #'
