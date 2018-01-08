845ae0f5a435869915d3a1b952549d68daffd3bb
John Coene <jcoenep@gmail.com>
2018-01-08 22:01:10 +0100

    to json force

--- R/utils.R
+++ R/utils.R
@@ -1,22 +1,24 @@
 build_opts_fp <- function(opts = NULL){
-  opts <- jsonlite::toJSON(opts, auto_unbox = T)
+  opts <- p_opts(opts)
 
   paste0("$(document).ready(function() {$('#fullpageshiny').fullpage(", opts, ");});")
 }
 
 build_opts_pp <- function(opts = NULL){
-
-  opts <- jsonlite::toJSON(opts, auto_unbox = T)
+  opts <- p_opts(opts)
 
   paste0("$(document).ready(function() {$('#fullpagePilling').pagepiling(", opts, ")});")
 }
 
 build_opts_mp <- function(opts = NULL){
-  opts <- jsonlite::toJSON(opts, auto_unbox = T)
+  opts <- p_opts(opts)
 
   paste0("$(document).ready(function() {$('#multiscrollshiny').multiscroll(", opts, ");});")
 }
 
+p_opts <- function(opts){
+  jsonlite::toJSON(opts, auto_unbox = TRUE, force = TRUE)
+}
 
 build_section <- function(menu = NULL, center = FALSE, class, ...){
   div <- shiny::tags$div(
